e7d8c404c48ca54f9924bf659e013037a37515f3
Stanislav Kondratyev <kondratjevsk@gmail.com>
2017-04-29 23:29:21 +0100

    Cleanup, event mode removed.

--- client/lclient.c
+++ client/lclient.c
@@ -1,4 +1,3 @@
-#include <curses.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -18,17 +17,22 @@
 #define LINE_CODE 2
 #define LINE_PART_CODE 3
 #define READ_ERROR_CODE 4
+#define FLUSH_ERROR_CODE 5
+#define WRITTEN_CODE 6
+#define WRITE_ERROR_CODE 7
 #define CMD_EXIT 0
 #define CMD_READ_CHAR 1
 #define CMD_READ_LINE 2
 #define CMD_PRINT_STDOUT 3
 #define CMD_PRINT_STDERR 4
-#define CMD_ORDER_MODE 5
-#define CMD_EVENT_MODE 6
 #define MAX_DATA_SIZE 1024
 #define HEADER_SIZE 3
-#define EV_KEY 5
 
+// = 0: OK
+// > 0: EOF
+// < 0: error (errno)
+// function readn
+// * Copyright (c) 1996 W. Richard Stevens.  All rights reserved.
 ssize_t                         /* Read "n" bytes from a descriptor. */
 readn(int fd, void *vptr, size_t n)
 {
@@ -53,6 +57,12 @@ readn(int fd, void *vptr, size_t n)
     return (n - nleft);         /* return >= 0 */
 }
 
+/* >=0: OK
+ * <0 : error, errno.
+ */
+/* function writen
+ * Copyright (c) 1996 W. Richard Stevens.  All rights reserved.
+ */
 /* Write "n" bytes to a descriptor. */
 ssize_t writen(int fd, const void *vptr, size_t n) {
     size_t nleft;
@@ -104,16 +114,28 @@ struct message {
     size_t data_len;
 };
 
+// < 0 on error, see errno.
 int send_message(int fd, struct message *msg) {
     msg->header[0] = msg->code;
     msg->header[1] = (msg->data_len >> 8) & 0xFF;
     msg->header[2] = msg->data_len % 0xFF;
-    if (writen(fd, msg->header, HEADER_SIZE) == -1) return -1;
-    if (writen(fd, msg->data, msg->data_len) == -1) return -2;
+    if (writen(fd, msg->header, HEADER_SIZE) < 0) return -1;
+    if (writen(fd, msg->data, msg->data_len) < 0) return -2;
     return 0;
 }
 
-void read_send_line(int fd, struct message *msg) {
+struct bag {
+    int fd;
+    struct message in_msg;
+    struct message out_msg;
+    char mode;
+    int exit_code;
+    fd_set rset;
+};
+
+
+
+int read_send_line(int fd, struct message *msg) {
     size_t len;
     bool proceed = true;
     while (proceed) {
@@ -128,49 +150,48 @@ void read_send_line(int fd, struct message *msg) {
                 msg->code = LINE_PART_CODE;
                 msg->data_len = len;
             }
-        } else if (feof(stdin)) {
-            msg->code = EOF_CODE;
-            msg->data_len = 0;
+        } else if (ferror(stdin)) {
+            msg->code = READ_ERROR_CODE;
+            int2bytes(errno, msg->data);
+            msg->data_len = sizeof(int);
             proceed = false;
         } else {
             msg->code = EOF_CODE;
             msg->data_len = 0;
             proceed = false;
         }
-        send_message(fd, msg);
     }
+    return send_message(fd, msg);
 }
 
-// retur number of bytes
-// 0: eof
-// -1: error
-int read_utf8_sequence(int fd, char *buf) {
-    // TODO use readn
-    int n = read(0, buf, 1);
-    if (n < 0) return - 1;
-    if (n == 0) return 0;
-    char first = buf[0];
+/*
+> 0: ok, bytes read
+0: eof
+-1: error
+*/
+int read_utf8_sequence(int fd, char *buf, int *err) {
+    int c = getchar();
+    if (c == EOF) goto input_error;
+    buf[0] = c;
     char more;
-    if (first >> 7 == 0) {
-        more = 0;
-    } else if (first >> 5 == 6) {
-        more = 1;
-    } else if (first >> 4 == 14) {
-        more = 2;
-    } else {
-        more = 3;
+    if (buf[0] >> 7 == 0) more = 0;
+    else if (buf[0] >> 5 == 6) more = 1;
+    else if (buf[0] >> 4 == 14) more = 2;
+    else more = 3;
+    for (int i = 0; i < more; ++i) {
+        c = getchar();
+        if (c == EOF) goto input_error;
+        buf[i] = c;
     }
-    if (more > 0) {
-        n = read(0, buf + 1, more);
-        if (n < 0) return -1;
-        if (n < more) return 0;
-        return 1 + more;
-    }
-    return 1;
+    return more + 1;
+input_error:
+    *err = errno;
+    return ferror(stdin) ? -1 : 0;
 }
 
-void read_send_utf8(int fd, struct message *msg) {
-    char n = read_utf8_sequence(fd, msg->data);
+int read_send_utf8(int fd, struct message *msg) {
+    int err;
+    int n = read_utf8_sequence(fd, msg->data, &err);
     if (n > 0) {
         msg->code = CHARACTER_CODE;
         msg->data_len = n;
@@ -178,13 +199,17 @@ void read_send_utf8(int fd, struct message *msg) {
         msg->code = EOF_CODE;
         msg->data_len = 0;
     } else {
-        // TODO: send errno in the data
         msg->code = READ_ERROR_CODE;
-        msg->data_len = 0;
+        int2bytes(err, msg->data);
+        msg->data_len = sizeof(int);
     }
-    send_message(fd, msg);
+    return send_message(fd, msg);
 }
 
+/*
+ * Never mind errno, if we can't read an order there's nothing we can do on our
+ * own; return a negative value & die asap.
+ */
 int read_order(int fd, struct message *msg) {
     int n = readn(fd, msg->header, HEADER_SIZE);
     if (n < HEADER_SIZE) return -1;
@@ -192,30 +217,33 @@ int read_order(int fd, struct message *msg) {
     msg->data_len = (unsigned char) msg->header[1] + (((unsigned char) msg->header[2]) << 8);
     if (msg->data_len > 0) {
         if (readn(fd, msg->data, msg->data_len) < msg->data_len) {
-            return -1;
+            return -2;
         }
     }
     return HEADER_SIZE + msg->data_len;
 }
 
-int read_event(struct message *msg) {
-    int ch = getch();
-    msg->code = EV_KEY;
-    msg->data_len = sizeof(int);
-    int2bytes(ch, msg->data);
-    return 0;
-}
-
-void print_stdout(int fd, struct message *msg) {
-    writen(1, msg->data, msg->data_len);
-    // in the future we won't flush immediately
-    fflush(NULL);
-}
-
-void print_stderr(int fd, struct message *msg) {
-    writen(2, msg->data, msg->data_len);
-    // in the future we won't flush immediately
-    fflush(NULL);
+int print_to_fd(struct bag *b, int output_fd) {
+    int err;
+    if (b->in_msg.data_len > 0) {
+        if (writen(output_fd, b->in_msg.data, b->in_msg.data_len) < 0) {
+            err = errno;
+            b->out_msg.code = WRITE_ERROR_CODE;
+            int2bytes(err, b->out_msg.data);
+            b->out_msg.data_len = sizeof(int);
+            return send_message(b->fd, &(b->out_msg));
+        }
+    }
+    if (fflush(NULL) == EOF) {
+        err = errno;
+        b->out_msg.code = FLUSH_ERROR_CODE;
+        int2bytes(err, b->out_msg.data);
+        b->out_msg.data_len = sizeof(int);
+        return send_message(b->fd, &(b->out_msg));
+    }
+    b->out_msg.code = WRITTEN_CODE;
+    b->out_msg.data_len = 0;
+    return send_message(b->fd, &(b->out_msg));
 }
 
 int bytes2int(char *bytes) {
@@ -226,59 +254,18 @@ int bytes2int(char *bytes) {
     return n;
 }
 
-struct bag {
-    int fd;
-    struct message in_msg;
-    struct message out_msg;
-    char mode;
-    int exit_code;
-    fd_set rset;
-};
-
-
-void init_event_mode() {
-    initscr();
-    raw();
-    keypad(stdscr, TRUE);
-    noecho();
-}
-void cleanup_event_mode() {
-    endwin();
-}
-
-void dispatch_order_event(struct bag *b) {
+int dispatch_order(struct bag *b) {
     char code = b->in_msg.code;
     if (code == CMD_EXIT) {
         b->mode = 'q';
         b->exit_code = bytes2int(b->in_msg.data);
+        return 0;
     }
-    else if (code == CMD_READ_LINE) read_send_line(b->fd, &(b->out_msg));
-    else if (code == CMD_READ_CHAR) read_send_utf8(b->fd, &(b->out_msg));
-    else if (code == CMD_PRINT_STDOUT) print_stdout(b->fd, &(b->in_msg));
-    else if (code == CMD_PRINT_STDERR) print_stderr(b->fd, &(b->in_msg));
-    else if (code == CMD_EVENT_MODE) {
-        if (b->mode != 'e') {
-            b->mode = 'e';
-            init_event_mode();
-        }
-    }
-    else if (code == CMD_ORDER_MODE) {
-        if (b->mode == 'e') cleanup_event_mode();
-        b->mode = 'o';
-    }
-    else if (code == EV_KEY) send_message(b->fd, &(b->in_msg));
-    else exit(EXIT_FAILURE);
-}
-
-void read_order_event(int fd, struct message *msg, fd_set *rset) {
-    FD_SET(0, rset);
-    FD_SET(fd, rset);
-    select(fd, rset, NULL, NULL, NULL);
-    if (FD_ISSET(fd, rset)) {
-        read_order(fd, msg);
-    } else {
-        read_event(msg);
-    }
+    if (code == CMD_READ_LINE) return read_send_line(b->fd, &(b->out_msg));
+    if (code == CMD_READ_CHAR) return read_send_utf8(b->fd, &(b->out_msg));
+    if (code == CMD_PRINT_STDOUT) return print_to_fd(b, 1);
+    if (code == CMD_PRINT_STDERR) return print_to_fd(b, 2);
+    return -1;
 }
 
 int main(int argc, char **argv) {
@@ -298,25 +285,10 @@ int main(int argc, char **argv) {
 
     connect(b.fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
-    /*
-    set_message(&msg, LINE_PART_CODE, "hello ", 6);
-    send_message(sockfd, &msg);
-
-    */
-    //set_message(&msg, LINE_CODE, "world", 5);
-    //send_message(sockfd, &msg);
-
-    //read_send_line(sockfd);
-    //read_send_utf8(sockfd);
-    
-
-    // not working?
-    int n;
     while (b.mode != 'q') {
-        if (b.mode == 'o') read_order(b.fd, &(b.in_msg));
-        else if (b.mode == 'e') read_order_event(b.fd, &(b.in_msg), &(b.rset));
-        dispatch_order_event(&b);
+        if (read_order(b.fd, &(b.in_msg)) < 0) return EXIT_FAILURE;
+        if (dispatch_order(&b) < 0) return EXIT_FAILURE;
     }
-    // communication error
+
     return b.exit_code;
 }
