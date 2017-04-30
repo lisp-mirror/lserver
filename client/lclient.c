eca4a0913fdf6d5d700df340e2c0f5fca7349d4b
Stanislav Kondratyev <kondratjevsk@gmail.com>
2017-04-30 01:50:56 +0100

    read_send_line: fixed

--- client/lclient.c
+++ client/lclient.c
@@ -164,7 +164,7 @@ int read_send_line(int fd, struct message *msg) {
             msg->data_len = 0;
             proceed = false;
         }
-    if (send_message(fd, msg) < 0); return -1;
+    if (send_message(fd, msg) < 0) return -1;
     }
     return 0
 }
