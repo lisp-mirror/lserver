05706b1a6f0c6dc9271e6b8f299b126502a86e67
Stanislav Kondratyev <kondratjevsk@gmail.com>
2017-07-07 12:15:49 +0100

    Export a few symbols.

--- server/package.lisp
+++ server/package.lisp
@@ -1,5 +1,19 @@
 ;;;; package.lisp
 
 (defpackage #:lserver-streams
-  (:use #:cl))
-
+  (:use #:cl)
+  (:export #:make-stream-with-lock
+           #:make-session-input-stream
+           #:make-session-output-stream
+           #:*buffer-size*
+           #:*header-size*
+           #:*small-buffer-size*
+           #:communication-error 
+           #:unknown-message-type-code
+           #:broken-pipe
+           #:corrupt-data  
+           #:corrupt-header
+           #:type-mismatch
+           #:client-error
+           #:flush-error
+           #:write-error))
